e98781f78fb6d1774ba8fd06f68c9aa7ec3a981b
Dustin Rouillard <me@dustin.sh>
2020-12-28 01:56:47 -0700

    Update for meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,28 @@ export default function Home(props: { playing: any }) {
       {props.playing.is_playing && <Spotify playing={props.playing} />}
       <div className="container">
         <Head>
-          <title>Dustin Rouillard</title>
+          <title>Dustin Rouillard • Vibing</title>
+
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <meta name="author" content="Dustin Rouillard" />
+          <meta name="copyright" content="Dustin Rouillard" />
+          <meta name="rating" content="General" />
+          <meta name="url" content="https://dustin.sh" />
+          <meta
+            name="description"
+            content="Dustin Rouillard - Software Engineer, Networking/Systems Administrator"
+          />
+          <meta name="twitter:creator" content="@dustinrouillard" />
+          <meta name="twitter:site" content="@dustinrouillard" />
+          <meta
+            name="keywords"
+            content="Software Engineer, Networking/Systems Administrator, Developer"
+          />
+
           <link
             rel="icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋🏻</text></svg>"
