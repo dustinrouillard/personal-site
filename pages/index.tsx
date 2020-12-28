dd99e8341720661c7ad159de283098e47ded5f47
Dustin Rouillard <me@dustin.sh>
2020-12-28 01:51:18 -0700

    Adding different favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,10 @@ export default function Home(props: { playing: any }) {
       <div className="container">
         <Head>
           <title>Dustin Rouillard</title>
-          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋🏻</text></svg>"
+          />
         </Head>
 
         <main>
