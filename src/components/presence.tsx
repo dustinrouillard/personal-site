27086b1f809859cfe5b41eefcd28e51e17757120
Dustin Rouillard <code@dstn.to>
2022-11-25 01:49:04 -0700

    Fixing broke types

--- src/components/presence.tsx
+++ src/components/presence.tsx
@@ -1,48 +1,44 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-import { InternalPlayerResponse } from "../types/gateway";
-import { LanyardActivity, LanyardPresence } from "../types/lanyard";
-import { gateway } from "../utils/gateway";
-import { lanyard } from "../utils/lanyard";
-import { Spotify } from "./spotify";
-import { VSCode } from "./vscode";
+import { SpotifyPlayingData } from '../types/gateway';
+import { LanyardActivity, LanyardPresence } from '../types/lanyard';
+import { gateway } from '../utils/gateway';
+import { lanyard } from '../utils/lanyard';
+import { Spotify } from './spotify';
+import { VSCode } from './vscode';
 
 export function Presence() {
-  const [spotify, setSpotify] = useState<InternalPlayerResponse>();
+  const [spotify, setSpotify] = useState<SpotifyPlayingData>();
   const [vsCode, setVsCode] = useState<LanyardActivity>();
   const [listening, setListening] = useState<boolean>(false);
 
   function presenceChange(data: LanyardPresence) {
-    const vscode = data.activities.find(
-      (activity) => activity.application_id == "383226320970055681"
-    );
+    const vscode = data.activities.find((activity) => activity.application_id == '383226320970055681');
     if (vscode) setVsCode(vscode);
     else setVsCode(null);
   }
 
   useEffect(() => {
-    const listener = (data: InternalPlayerResponse) => {
-      if ("is_playing" in data) setListening(data.is_playing);
+    const listener = (data: SpotifyPlayingData) => {
+      if ('playing' in data) setListening(data.playing);
       setSpotify((state) => {
         return { ...state, ...data };
       });
     };
 
-    gateway.on("spotify", listener);
-    lanyard.on("presence", presenceChange);
+    gateway.on('spotify', listener);
+    lanyard.on('presence', presenceChange);
 
     return () => {
-      gateway.removeListener("spotify", listener);
-      lanyard.removeListener("presence", presenceChange);
+      gateway.removeListener('spotify', listener);
+      lanyard.removeListener('presence', presenceChange);
     };
   }, []);
 
   return (
     <WrappedPresence>
-      {listening && (
-        <Spotify spotify={spotify} listening={listening} small={!!vsCode} />
-      )}
+      {listening && <Spotify spotify={spotify} listening={listening} small={!!vsCode} />}
       {!!vsCode && listening && <Right />}
       {!!vsCode && <VSCode small={listening} vscode={vsCode} />}
     </WrappedPresence>
