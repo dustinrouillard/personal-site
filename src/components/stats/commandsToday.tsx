14ebd4c1e291bd2c96cfb55c4885ac3980209fbb
Dustin Rouillard <code@dstn.to>
2024-10-26 13:37:20 -0600

    Changing cli commands to be more descriptive

--- src/components/stats/commandsToday.tsx
+++ src/components/stats/commandsToday.tsx
@@ -1,4 +1,5 @@
 import { useAnalytics } from "../../hooks/useAnalytics";
+import { Tippy } from "../tippy";
 
 interface Props {
   className: string;
@@ -11,10 +12,16 @@ export function CommandsToday({ className }: Props) {
     <div className={className}>
       <div className="relative text-black bg-neutral-200/50 dark:text-white dark:bg-neutral-800 rounded-md w-auto h-40 p-4 flex flex-col space-y-6 justify-center items-center">
         <span />
-        <p className="text-3xl font-bold mb-8 mx-20">
-          {analytics.commands.day.toLocaleString() ?? "--"}
-        </p>
-        <p className="text-nowrap text-center">Commands last 24h</p>
+        <Tippy
+          placement="auto"
+          content="# of shell commands ran in the last 24h"
+        >
+          <p className="text-3xl font-bold mb-8 mx-20">
+            {analytics.commands.day.toLocaleString() ?? "--"}
+            <span className="opacity-40">/24h</span>
+          </p>
+        </Tippy>
+        <p className="text-nowrap text-center">CLI Commands</p>
       </div>
     </div>
   ) : (
