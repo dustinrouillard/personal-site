4f9143cb5e14426c2e838869b64b721eb74eda7a
Dustin Rouillard <code@dstn.to>
2025-05-07 14:10:40 -0600

    Move favicon stuff to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import PlausibleProvider from "next-plausible";
 import { Toaster, ToastIcon, resolveValue } from "react-hot-toast";
 
 import "../assets/app.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -13,12 +14,19 @@ export default function App({ Component, pageProps }) {
       }
       selfHosted
     >
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Noto+Color+Emoji&family=Coda&display=swap"
-        rel="stylesheet"
-      />
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋🏻</text></svg>"
+        />
+        <link rel="icon" type="image/png" href="/favicon.png" sizes="64x64" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Noto+Color+Emoji&family=Coda&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
 
       <Toaster containerClassName="group">
         {(t) => (
--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -59,12 +59,6 @@ export default function Layout(props: Props) {
         <title>Dustin Rouillard</title>
 
         <Meta />
-
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋🏻</text></svg>"
-        />
-        <link rel="icon" type="image/png" href="/favicon.png" sizes="64x64" />
       </Head>
 
       {christmasTime ? (
