7bda4d6b7809ec2360569707b4a591432ca34df8
Dustin Rouillard <code@dstn.to>
2025-07-29 10:55:02 -0600

    Change query param because cached 404s

--- src/components/ImageThumbnail.tsx
+++ src/components/ImageThumbnail.tsx
@@ -3,7 +3,7 @@
 import NextImage, { ImageLoaderProps, ImageProps } from "next/image";
 
 const imageProxyLoader = ({ src, width, quality }: ImageLoaderProps) => {
-  return `https://proxy.t.pics/${width},q${quality || 75}/${src}?.`;
+  return `https://proxy.t.pics/${width},q${quality || 75}/${src}?-`;
 };
 
 export const ImageThumbnail = (props: ImageProps) => {
